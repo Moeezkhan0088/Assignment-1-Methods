2066a358079e367aa35b60c4956aa918424c33c9
Muhammad Moeez Khan <162406444+Moeezkhan0088@users.noreply.github.com>
2024-08-18 21:31:43 +0500

    assignment 1 number to string

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// const a = Number("5")
+// const b = Number("3")
+
+// const sum = (a: number, b: number)=>{
+//     return a + b;
+// };
+// console.log(sum(a, b));
+
+// const multiply = (a: number, b: number)=>{
+//     return a * b;
+// };
+
+// console.log(multiply(a, b));
+
+
+let num: number = 20;
+let str: string = num.toString();
+
+
+console.log(typeof(str));
+
+ console.log(str);
